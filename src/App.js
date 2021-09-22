44ca13b893fd75508299ac4a40acd8aef278d655
Raghad-Mezied <rmezied3@gmail.com>
2021-09-22 08:55:06 +0300

    create the routes Relates #3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route exact path="/levels"></Route>
+          <Route exact path="/game"></Route>
+          <Route exact path="/statistics"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
